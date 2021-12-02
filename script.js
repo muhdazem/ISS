b9df51f98b955d4b524c91474ef42a640168d8e5
Muhd Azem <56638238+muhdazem@users.noreply.github.com>
2021-12-02 21:08:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   id: 'mapbox/streets-v11',
   tileSize: 512,
   zoomOffset: -1,
-  accessToken: 'your.mapbox.access.token'
+  accessToken: 'pk.eyJ1IjoiYWthc2hpOTciLCJhIjoiY2t3b3owOWp5MDdxMDJvbThwcW1mOXd5OSJ9.0guLnNaxBOWVjyEwFW-sNg'
 }).addTo(map);
 
 
